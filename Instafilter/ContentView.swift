18bfbd4e3a56ae2a5af7d34a8f9b1697e1f8587d
Juliette Rapala <jrapala@gmail.com>
2020-11-30 16:13:45 -0600

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Juliette Rapala on 11/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Juliette Rapala on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
