29f7d9e56444389be99341b3d3f2f01465f1023a
Juliette Rapala <jrapala@gmail.com>
2020-12-01 22:41:44 -0600

    feat: add UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+
     var body: some View {
-        VStack {
-            Text("Hello World")
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+                    
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    // select an image
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: self.$filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save") {
+                        // save the picture
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
         }
     }
 }
