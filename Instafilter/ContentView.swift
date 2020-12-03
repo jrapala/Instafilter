c1253f700dbf8bd155d59bfa634c6885b2f4f8ab
Juliette Rapala <jrapala@gmail.com>
2020-12-02 18:36:05 -0600

    feat: load image and add filter

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    // context is an object responsible for rendering a CIImage to a CGImage
+    // They're expensive to make, so make one and keep it alive
+    let context = CIContext()
 
     var body: some View {
         NavigationView {
@@ -62,7 +68,24 @@ struct ContentView: View {
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        // set the filter intensity
+        currentFilter.intensity = Float(filterIntensity)
+        
+        // get the output image
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        // ask CIContext to render image and place the result in image property, so it appears on screen
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
